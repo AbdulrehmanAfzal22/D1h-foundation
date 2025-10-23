bca80d8c276e0bde013d4958c31e74d9f7d28d0b
AbdulrehmanAfzal22 <abdulrehmanafzal60@gmail.com>
2025-10-23 21:57:47 +0500

    Landing page

---
+++ script.js
@@ -0,0 +1,10 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function changeSlide() {
+  slides[currentSlide].classList.remove('active');
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add('active');
+}
+
+setInterval(changeSlide, 10000); 
